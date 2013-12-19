4b14ec7ab678ab746ed75f7fb2bc8b8034834330
vdcraats <vdcraats@online.nl>
2013-12-19 15:45:24 +0100

    config aangepast

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ultrapip/';
+$config['base_url']	= 'http://localhost/diplopoda/ultrapip/';
 
 /*
 |--------------------------------------------------------------------------
